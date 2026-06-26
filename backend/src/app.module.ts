c2efb1ac040b6ca719b254e408b6194f46b06e4b
giihsatiko <giovana_satiko@hotmail.com>
2026-06-26 20:53:08 -0300

    chore: initial commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { AppointmentsModule } from './appointments/appointments.module';
+
+@Module({
+  imports: [PrismaModule, AppointmentsModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
